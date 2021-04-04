434b6a7a6161bb77a37bb7813c1283ac736eb489
tbrkcbn <tugberkcoban@gmail.com>
2021-04-04 21:36:43 +0300

    Initial commit

---
+++ src/test/java/com/tbrkcbn/commercial/CommercialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbrkcbn.commercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
